0078dbaa30b3d0878e40ac4835b5a5bfb02373ec
EHeisey <EHeisey@users.noreply.github.com>
2017-05-10 12:17:32 -0400

    Add files via upload

--- Piece.java
+++ Piece.java
@@ -7,15 +7,19 @@ package chess;
 public abstract class Piece {
     
     private Board board;
+    private Color color;
     
-    public Piece(Board b){
+    public Piece(Board b, Color c){
         board = b;
+        color = c;
     }
     
     public Board getBoard(){
         return board;
     }
     
-    
+    public Color getColor(){
+        return color;
+    }
     
 }
