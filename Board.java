73b6d0cae076028a8ee9e5efab66c480bf40f33f
squaredragon <squaredragon@users.noreply.github.com>
2017-05-17 13:04:16 -0400

    Update Board.java

--- Board.java
+++ Board.java
@@ -16,5 +16,20 @@ public class Board {
             }
         }
     }
+    public void printBoard() {
+        System.out.println();
+        for (int i = 0; i<8; i++) {
+            System.out.print(i+1);
+            for (int j = 0; j<8; j++) {
+                 if (board[i][j].getPiece() == null) {
+                  System.out.print(" _");
+                 } else {
+                     System.out.print(" " + board[i][j].getPiece().getSymbol());                          
+                 } 
+            }    
+            System.out.println();
+        }
+        System.out.println("\n a b c d e f g h");
+    }
     
 }
